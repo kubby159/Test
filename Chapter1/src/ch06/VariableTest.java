214ebca58c5cbce05c2237b25c8308fa3e49dbf6
kubby <kubby@DESKTOP-LTACJ49>
2022-03-31 17:28:01 +0900

    Second commiit!

---
+++ Chapter1/src/ch06/VariableTest.java
@@ -0,0 +1,23 @@
+package ch06;
+
+public class VariableTest {
+
+	public static void main(String[] args) {
+		
+		//byte는 -127~ 127까지만 사용가능함
+		byte bNum = 127;
+		System.out.println(bNum);
+		//int INum = 1234567890123123213;
+		long lNum = 123L;
+		System.out.println(lNum);
+		
+		double dNum = 3.14;
+		float fNum = 3.14f;
+		System.out.println(dNum);
+		System.out.println(fNum);
+		
+
+		
+	}
+
+}
