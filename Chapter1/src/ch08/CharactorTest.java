91d5adb11f39cec24595e841ac26c13ffbebe20c
kubby <kubby@DESKTOP-LTACJ49>
2022-03-31 22:20:07 +0900

    CharactorTest

---
+++ Chapter1/src/ch08/CharactorTest.java
@@ -0,0 +1,16 @@
+package ch08;
+
+public class CharactorTest {
+
+	public static void main(String[] args) {
+		
+		char ch1 = 'A';
+		char aNum = 66; //'B'
+		System.out.println(ch1);
+		System.out.println(aNum);
+		
+		int ch3 = 67;
+		System.out.println((char)ch3); //'C'
+	}
+
+}
