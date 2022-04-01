809ce9bc04ecbe1112916ab3d0e4b068f5d1b693
kubby <kubby159@naver.com>
2022-04-01 14:56:15 +0900

    test

--- Chapter1/src/ch12/LogicalTest.java
+++ Chapter1/src/ch12/LogicalTest.java
@@ -5,7 +5,7 @@ public class LogicalTest {
 	public static void main(String[] args) {
 
 		int num1 = 10;
-		int num2 = 2;
+		int num2 = 3;
 		
 		boolean result = ((num1+=10) < 10 );
 		System.out.println(result);
