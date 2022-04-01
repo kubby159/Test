7fd45ae2246cd284f8e4e3b15cfe2b36841f1946
kubby <kubby@DESKTOP-LTACJ49>
2022-04-01 14:47:33 +0900

    boolean logical

---
+++ Chapter1/src/ch12/LogicalTest.java
@@ -0,0 +1,14 @@
+package ch12;
+
+public class LogicalTest {
+
+	public static void main(String[] args) {
+
+		int num1 = 10;
+		int num2 = 2;
+		
+		boolean result = ((num1+=10) < 10 );
+		System.out.println(result);
+	}
+
+}
