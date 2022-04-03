fd53ac2f76b4f2ed4f27ed7ba162e37386d41831
Jay <kubby159@naver.com>
2022-04-03 12:59:33 +0900

    for

---
+++ Chapter1/src/ch17/ForTest.java
@@ -0,0 +1,26 @@
+package ch17;
+
+public class ForTest {
+
+	public static void main(String[] args) {
+
+		int sum = 0;
+		for (int i = 0; i <= 10; i++) {
+
+			sum += i;
+			System.out.println(i);
+		}
+		System.out.println(sum);
+
+		int num = 1;
+		int total = 0;
+
+		while (num <= 10) {
+			total += num;
+			num++;
+		}
+
+		System.out.println(total);
+	}
+
+}
